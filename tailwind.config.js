7f2baa9792a8f473b64efa53e1470ddfb0a3e6ca
P541M <videna.psalmeleazar@gmail.com>
2024-08-11 18:51:14 -0400

    feat: new colour theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-blue": "#1E3A8A", // Deep Blue
+        "light-blue": "#3B82F6", // Light Blue
+        "gray-dark": "#4B5563", // Dark Gray
+        "gray-light": "#D1D5DB", // Light Gray
+        "success-green": "#10B981", // Success Green
+      },
+    },
   },
   plugins: [],
 };
